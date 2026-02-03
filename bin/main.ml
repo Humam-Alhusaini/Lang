341adea514acc7586586601a216c2878ba09ab85
Humam Alhusaini <humamalhusaini@tutamail.com>
2026-02-03 13:06:32 -0600

    improved error handling, and reorganized funcs

--- bin/main.ml
+++ bin/main.ml
@@ -10,18 +10,20 @@ let interp str =
       let tokens = lex#tokenize [] in 
       let _ = print_tokens (toks_to_tokens tokens) in
         let parse = new parse_exp tokens in
-        let _ = parse#parse_expr in ()
+          let ast = parse#parse_expr in 
+            printf "\n%s\n" (fexpr ast) 
   with 
   | Lexing_error (err, toks, pos) -> 
       printf "LEXING ERROR at line %d, offset %d: %s\n\n\n" pos.line_num pos.bol_off err;
       print_string "Printing retrieved tokens...\n\n";
-      print_tokens toks;;
+      print_tokens toks
+  | Parsing_error (err, tok) -> printf "\n"; printf "%s, got %s\n" err (format_token tok);;
 
 let rec repl () =
   print_string ">>> ";
   let txt = read_line () in
   match txt with
   | "exit" -> print_endline "Goodbye!"
-  | _ -> interp txt
+  | _ -> interp txt; repl ()
 
 let () = repl ();;
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,7 +1,47 @@
-exception Parsing_error of string
 
 open Lexer
 open Tokens
+open Printf
+
+let format_tok (tok : Tokens.t) = 
+  match tok with
+  | NUM i -> sprintf "NUM(%i)" i
+  | VAR s -> sprintf "VAR(%s)" s
+  | MULT -> "MULT"
+  | PLUS -> "PLUS"
+  | SUB -> "SUB"
+  | EQ -> "EQ"
+  | LPAREN -> "LPAREN"
+  | RPAREN -> "RPAREN"
+  | LBRACE -> "LBRACE"
+  | RBRACE -> "RBRACE"
+  | LBRACK -> "LBRACK"
+  | RBRACK -> "RBRACK"
+  | SEMICOLON -> "SEMICOLON"
+  | COLON -> "COLON"
+  | AND -> "AND"
+  | OR -> "OR"
+  | IF -> "IF"
+  | ELSE -> "ELSE"
+  | TRUE -> "TRUE"
+  | FALSE -> "FALSE"
+  | COMMA -> "COMMA"
+  | PERIOD -> "PERIOD"
+  | NAT -> "NAT"
+  | EOF -> "EOF";;
+
+let format_pos (pos : position) : string = 
+  sprintf "line %d, offset %d" pos.line_num pos.bol_off;;
+
+let format_token (token : token) : string = 
+  let (tok, pos) = token in
+    sprintf "%s at %s" (format_tok tok) (format_pos pos);;
+
+let error_of_token (err: string) (token : token) : string =
+  sprintf "%s, got %s" err (format_token token);;
+
+exception Parsing_error of string * token
+exception Fatal of string
 
 type expr = 
   | Num of int
@@ -12,13 +52,25 @@ and op =
   | Sub
   | Mult
 
+ let match_num n =
+   match n with
+   | (NUM y, _) -> Num y
+   | _ -> Parsing_error ("Expected num", n) |> raise;;
+
+ let match_op op = 
+   match op with
+   | (MULT, _) -> Mult
+   | (PLUS, _) -> Add
+   | (SUB, _) -> Sub
+   | _ -> Parsing_error ("Expected operator", op) |> raise;;
+
 class parse_exp (tokens : token list) = object (self)
   
   val mutable toks = tokens
 
   method shift () =
    match toks with
-   | [] -> Parsing_error "No more" |> raise
+   | [] -> Fatal "FATAL ERROR" |> raise
    | _ :: ls -> toks <- ls
 
   method shift_n num =
@@ -30,24 +82,15 @@ class parse_exp (tokens : token list) = object (self)
   method parse_expr : expr = 
     
     let rec parse_binop (start : expr) : expr =
-      
-        let match_op op = 
-          match op with
-          | MULT -> Mult
-          | PLUS -> Add
-          | SUB -> Sub
-          | _ -> Parsing_error "I expect an operator between 2 numbers!" |> raise
-           in
-
       match toks with
-      | ((MULT | PLUS | SUB) as op, _) :: (NUM y, _) :: _ -> self#shift_n 2; Binop(match_op op, start, Num y) |> parse_binop
+      | op :: num :: _ -> self#shift_n 2; Binop(match_op op, start, match_num num) |> parse_binop
       | [] -> start
-      | _ -> Parsing_error "What the helly is happening here?" |> raise
+      | hd :: _ -> Parsing_error ("Expected expression to either end or continue", hd) |> raise
        in
     
     match toks with
-    | (NUM x, _) :: _ -> self#shift (); parse_binop (Num x) 
-    | [] -> Parsing_error "Where the helly are the tokens" |> raise
-    | _ -> Parsing_error "Anomalous op" |> raise
+    | (NUM y, _) :: _ -> self#shift (); parse_binop (Num y) 
+    | hd :: _ -> Parsing_error ("Expected num", hd) |> raise
+    | [] -> Fatal "Where the helly are the tokens" |> raise
 
 end
